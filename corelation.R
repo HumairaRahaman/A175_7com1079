a0e46142863a0bdeb8081b3da41857c10cf6cfb9
Muhammad Nouman Shafiq <ms24aga@herts.ac.uk>
2024-11-25 02:16:21 +0000

    Fix: fix the value_names

--- corelation.R
+++ corelation.R
@@ -17,25 +17,24 @@ hist(df4$happiness_score,
      freq = FALSE)  # Use density for y-axis to overlay curve
 
 # Normal distribution curve
-curve(dnorm(x, mean = mean(df2$happiness_score), sd = sd(df2$happiness_score)), 
+curve(dnorm(x, mean = mean(df4$happiness_score), sd = sd(df4$happiness_score)), 
       col = "red", 
       lwd = 2, 
       add = TRUE)
 
 # Correlation test using Pearson
-correlation_result <- cor.test(df2$happiness_score, df2$gdp_per_capita, method = "pearson")
+correlation_result <- cor.test(df4$happiness_score, df4$gdp_per_capita, method = "pearson")
 # Correlation test result
-print("Correlation test =")
 print(correlation_result)
 
 # Scatterplot with linear trendline
-plot(df2$gdp_per_capita, df2$happiness_score,
+plot(df4$gdp_per_capita, df4$happiness_score,
      xlab = "GDP Per Capita", 
      ylab = "Happiness Score", 
      main = "Scatterplot of GDP Per Capita vs Happiness Score",
      col = "blue", 
      pch = 16)
 # Linear trend line to the scatterplot
-abline(lm(happiness_score ~ gdp_per_capita, data = df2), col = "red", lwd = 2)
+abline(lm(happiness_score ~ gdp_per_capita, data = df4), col = "red", lwd = 2)
 
 
